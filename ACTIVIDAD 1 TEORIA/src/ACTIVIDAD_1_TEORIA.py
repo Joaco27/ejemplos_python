1c532cbdef6bbd6b9233e89570c2f9fc8f354bcf
Joaco27 <81045279+Joaco27@users.noreply.github.com>
2021-05-03 15:22:49 -0300

    Update ACTIVIDAD_1_TEORIA.py

--- ACTIVIDAD 1 TEORIA/src/ACTIVIDAD_1_TEORIA.py
+++ ACTIVIDAD 1 TEORIA/src/ACTIVIDAD_1_TEORIA.py
@@ -1,19 +1,4 @@
 
-import csv
-import json
-
-
-
-
-def Apps():
-    arch = open('D:/Facultad/Seminario Pythonga/common_apps.csv', 'r')
-    csvreader = csv.reader(arch, delimiter=',')
-    encabezado = next(csvreader)
-    print(encabezado)
-
-
-
-
 
 from src.components import menuInicio
 
